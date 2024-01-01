325b734daf9229d99d8422608857109a8fbe9793
Meik Kurde <43165848+kurdem@users.noreply.github.com>
2024-01-01 11:21:30 +0100

    Update 02_install_ansible.sh

--- 02_install_ansible.sh
+++ 02_install_ansible.sh
@@ -1,8 +1,12 @@
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-#sudo apt-get install ansible
+
 sudo apt install python3-pip
+sudo apt install python3-dev
+# pywinrm is a Python client for the Windows Remote Management (WinRM) service
 sudo pip3 install pywinrm
+# pyVmomi is the Python SDK for the VMware vSphere API that allows you to manage ESX, ESXi, and vCenter
 sudo pip3 install pyvmomi
 sudo pip3 install ansible
+# Ansible Azure Module
 sudo pip3 install ansible[azure]
